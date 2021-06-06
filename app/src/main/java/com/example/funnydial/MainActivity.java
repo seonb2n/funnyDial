5d6501d876d20779e8026f298f88bf4a11380bc9
seonb2n <626ksb@gmail.com>
2021-06-06 10:45:01 +0900

    프로젝트 생성 및 레이아웃 생성

---
+++ app/src/main/java/com/example/funnydial/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.funnydial;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView1 = (TextView)findViewById(R.id.textViewOne);
+        TextView textView2 = (TextView)findViewById(R.id.textViewTwo);
+        SeekBar seekBar1 = (SeekBar)findViewById(R.id.seekBar1);
+        SeekBar seekBar2 = (SeekBar)findViewById(R.id.seekBar2);
+
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView1.setText(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textView1.setTextColor(Color.parseColor("#4169E1"));
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView1.setTextColor(Color.parseColor("#000000"));
+            }
+        });
+
+    }
+}
